6944b87d2b14dbf3ff02e9dcbac6600ee6c827ef
masad-frost <farismasad@gmail.com>
2020-09-23 13:21:21 -0700

    Tests generate a new token for each test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,55 @@
 /* eslint-env jest */
 
+import * as crypto from 'crypto';
 import { Client } from '..';
 
 // eslint-disable-next-line
 const WebSocket = require('ws');
 
-const REPL_TOKEN = process.env.REPL_TOKEN as string;
+const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
 
-if (!REPL_TOKEN) {
-  throw new Error('REPL_TOKEN is required');
+if (!TOKEN_SECRET) {
+  throw new Error('TOKEN_SECRET is required to run tests');
+}
+
+function genToken() {
+  const opts = {
+    id: `testing-crosis-${Math.random().toString(36).split('.')[1]}`,
+    mem: 1024 * 1024 * 512,
+    thread: 0.5,
+    share: 0.5,
+    net: true,
+    attach: true,
+    bucket: 'test-replit-repls',
+    ephemeral: true,
+    nostore: true,
+    language: 'bash',
+    owner: true,
+    path: Math.random().toString(36).split('.')[1],
+    disk: 1024 * 1024 * 1024,
+    bearerName: 'crosistest',
+    bearerId: 2,
+    presenced: true,
+    user: 'crosistest',
+    pullFiles: true,
+    polygott: false,
+    format: 'pbuf',
+  };
+  const encodedOpts = Buffer.from(
+    JSON.stringify({
+      created: Date.now(),
+      salt: Math.random().toString(36).split('.')[1],
+      ...opts,
+    }),
+  ).toString('base64');
+
+  const hmac = crypto.createHmac('sha256', TOKEN_SECRET);
+  hmac.update(encodedOpts);
+  const msgMac = hmac.digest('base64');
+
+  const token = Buffer.from(`${encodedOpts}:${msgMac}`);
+
+  return token.toString('base64');
 }
 
 jest.setTimeout(10 * 1000);
