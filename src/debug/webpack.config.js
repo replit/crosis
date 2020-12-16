68b86730d7d6dd607c41695bd095e243e1ea6673
Moudy <moudy.elkammash@gmail.com>
2020-12-16 18:45:51 +0000

    add source map

--- src/debug/webpack.config.js
+++ src/debug/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'source-map',
   entry: path.resolve(__dirname, 'debug.ts'),
   output: {
     filename: 'debug.js',
