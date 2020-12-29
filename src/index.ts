cf1eb5de49d04305ec54ddd07113e6bb02597c9c
masad-frost <farismasad@gmail.com>
2020-12-29 14:12:34 +0200

    No need to monkey patch protobufjs anymore https://github.com/protobufjs/protobuf.js/pull/1486 released in protobufjs 6.10.2 and updated in @replit/protocol in https://github.com/replit/protocol/pull/9 and released in 0.2.15

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
-import './util/utf8ReadMonkeypatch'; // pbjs's utf8 decoder is borked
 
 export { Client } from './client';
 export { Channel } from './channel';
