9f4b6d88b1103ff4d6b53deb65998900507f3b3a
masfrost <farismasad@gmail.com>
2022-05-11 16:00:53 -0700

    Add default retryCallback factory

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,17 +1,36 @@
 import * as urllib from 'url';
-import type { ConnectOptions, GovalMetadata } from '../types';
+import { ConnectionError, ConnectOptions, GovalMetadata, RetryCb } from '../types';
 
 const BACKOFF_FACTOR = 1.7;
 const MAX_BACKOFF = 15000;
 
-/**
- * Calculates the backoff for n retry
- */
-export function getNextRetryDelay(retryNumber: number): number {
+function getNextRetryDelay(retryNumber: number): number {
   const randomMs = Math.floor(Math.random() * 500);
   const backoff = BACKOFF_FACTOR ** retryNumber * 1000;
 
   return Math.min(backoff, MAX_BACKOFF) + randomMs;
+}
+
+export function createDefaultRetryCallback(): RetryCb {
+  let websocketFailureCount = 0;
+
+  return async function retryCallback({
+    count,
+    retryReason,
+  }: {
+    count: number;
+    retryReason: ConnectionError;
+  }) {
+    if (retryReason === ConnectionError.SocketClosure) {
+      websocketFailureCount += 1;
+    }
+
+    return {
+      shouldAbort: false,
+      backOffMs: getNextRetryDelay(count),
+      shouldPoll: websocketFailureCount > 3,
+    };
+  };
 }
 
 function isWebSocket(w: unknown): w is WebSocket {
