643ca27ed2aa0286f20db84b1df4faf776a2696a
Giuseppe Burtini <giuseppe@burtini.ca>
2023-07-02 12:55:23 -0700

    Add a second case.

--- src/__tests__/regressions.test.ts
+++ src/__tests__/regressions.test.ts
@@ -31,7 +31,7 @@ afterAll(() => {
   testingClients.forEach((c) => c.destroy());
 });
 
-concurrent("interlaced connections don't throw", async (done) => {
+concurrent("interlaced connections don't throw - abort during case", async (done) => {
   // This regression-tests a user-state achievable throw where you open, then during
   // connection metadata fetch, open, close, open, close and end up with an interlaced
   // connection state as a result of handleClose resetting the connection status.
@@ -70,7 +70,7 @@ concurrent("interlaced connections don't throw", async (done) => {
       throw new Error('Expected to never get here (abort+instant close).');
     }),
   );
-  // set up the initial abort
+
   client.close();
 
   client.open(
@@ -83,4 +83,57 @@ concurrent("interlaced connections don't throw", async (done) => {
   await abortingPromise;
 
   client.close();
+});
+
+concurrent("interlaced connections don't throw - abort after case", async (done) => {
+  // This isn't known to be user-achievable, but is roughly the same condition as the
+  // abort-during case, but with the abort after the final close..
+  // CrosisError: Expected abort returned from fetchConnectionMetadata to be truthy when the controller aborts
+
+  const waitingMetadata = () =>
+    new Promise<FetchConnectionMetadataResult>((resolve) => {
+      return getConnectionMetadata().then(resolve);
+    });
+
+  let abortingPromise: Promise<FetchConnectionMetadataResult> | null = null;
+  const abortingMetadata = () => {
+    if (abortingPromise) {
+      return abortingPromise;
+    }
+
+    abortingPromise = new Promise<FetchConnectionMetadataResult>((resolve) => {
+      resolve({
+        error: FetchConnectionMetadataError.Aborted,
+      });
+    });
+    return abortingPromise;
+  };
+
+  const client = getClient<{ username: string }>(done);
+  const ctx = { username: 'zyzz' };
+
+  const params = {
+    fetchConnectionMetadata: getConnectionMetadata,
+    WebSocketClass: WebSocket,
+    context: ctx,
+  };
+  client.open(
+    { ...params, fetchConnectionMetadata: abortingMetadata },
+    wrapWithDone(done, () => {
+      throw new Error('Expected to never get here (abort+instant close).');
+    }),
+  );
+
+  client.close();
+
+  client.open(
+    { ...params, fetchConnectionMetadata: waitingMetadata },
+    wrapWithDone(done, () => {
+      console.log('reached second client.open');
+    }),
+  );
+
+  client.close();
+
+  await abortingPromise;
 });
